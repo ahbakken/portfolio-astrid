9cf1009f4bc3bcd4d749f2b712e806adb723e9fe
Astrid Bakken <astridbakken95@gmail.com>
2023-01-08 17:01:07 +0100

    Navbar: sidebar menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,94 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import { FaLinkedinIn } from 'react-icons/fa';
 // rafce
 
 const Navbar = () => {
   return (
-    <div className="fixed w-full h-28 shadow-xl z-[108]">
-      <div className="flex justify-between items-center w-full px-2 2xl:px-16">
-      <Image src='/../public/assets/skills/js.png' height="10" width="100" alt="Logo" />
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image
+          src="/../public/assets/navLogo.png"
+          height="70"
+          width="70"
+          alt="/"
+        />
+        <div>
+          <ul className="hidden md:flex">
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Projects
+              </li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Contact
+              </li>
+            </Link>
+          </ul>
+          <div className="md:hidden">
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+
+      <div className="fixed left-0 top-0 w-full h-screen bg-black/50">
+        <div className="fixed left-0 top-0 w-[75%] sm:w[60%] md:w[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
+          <div>
+            <div className="flex w-full items-center justify-between ">
+              <Image
+                src="/../public/assets/navLogo.png"
+                width="70"
+                height="80"
+                alt="/"
+              />
+              <div className="rounded-full shadow-lg shadow-gray-400 p-2 cursor-pointer">
+                <AiOutlineClose size={12} />
+              </div>
+            </div>
+            <div className="border-b border-gray-300 my-4">
+              <p className="w-[85%] md:w-[90%] py-4">Welcome to my page!</p>
+            </div>
+          </div>
+          <div>
+            <ul>
+              <Link href="/">
+                <li className="py-4 text-sm">Home</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">About</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Skills</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Projects</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Contact</li>
+              </Link>
+            </ul>
+            <div className="pt-40">
+              <p className="uppercase tracking-widest text-[#5615e5]  ">
+                Feel free to contact me!
+              </p>
+              <div>
+                <FaLinkedinIn />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
